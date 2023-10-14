913f1e7be4af70170f473519238e4e216a4dab1b
magaliiiiiiii <magali.g@bue.edu.ar>
2023-10-14 20:50:11 -0300

    api aun sin disenio

---
+++ opinionescontantes.js
@@ -0,0 +1,13 @@
+const opinionesRestaurantes = [
+    {
+        restaurante: "AQVA RESTAURANTE",
+        calificacion: 4.5,
+        comentario: "Excelente comida y servicio."
+    },
+    {
+        restaurante: "HOLY BREWERY PUERTO IGUAZÚ",
+        calificacion: 3.8,
+        comentario: "Buen lugar, pero la comida podría mejorar."
+    },
+    // Agrega más opiniones aquí
+];
