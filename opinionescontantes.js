8bcad74931e4a1cfa127d10121cd78b139223082
magaliiiiiiii <magali.g@bue.edu.ar>
2023-10-23 20:56:43 -0300

    cambios

--- opinionescontantes.js
+++ opinionescontantes.js
@@ -26,3 +26,34 @@ let.header= `
 
 
 document.getElementsById("idheader").innerHTML= header
+
+document.addEventListener("DOMContentLoaded", function () {
+    const comentariosList = document.getElementById("comentarios-list");
+
+    // Hacer una solicitud a la API de JSONPlaceholder para obtener comentarios
+    fetch("https://jsonplaceholder.typicode.com/comments")
+        .then((response) => response.json())
+        .then((comentarios) => {
+            // Tomar solo los primeros dos comentarios
+            const comentariosLimitados = comentarios.slice(0, 2);
+
+            // Recorrer los comentarios limitados y agregarlos a la lista en la página
+            comentariosLimitados.forEach((comentario) => {
+                // Hacer una solicitud adicional para obtener el nombre de usuario del comentario
+                fetch(`https://jsonplaceholder.typicode.com/users/${comentario.id}`)
+                    .then((response) => response.json())
+                    .then((usuario) => {
+                        const listItem = document.createElement("li");
+
+                        // Agregar una clase CSS al elemento <li>
+                        listItem.classList.add("comentarios"); // Reemplaza "mi-clase-css" con el nombre de tu clase CSS
+
+                        listItem.innerHTML = `<strong>${usuario.name}</strong>: ${comentario.body}`;
+                        comentariosList.appendChild(listItem);
+                    });
+            });
+        })
+        .catch((error) => {
+            console.error("Error al cargar los comentarios: " + error);
+        });
+});
