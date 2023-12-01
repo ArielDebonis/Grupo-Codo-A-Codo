25a9f5b98f4a05f7fae6afbf9af96fc26fbfe31b
jenndiaz97 <jenndiaz1307@gmail.com>
2023-12-01 18:13:17 -0300

    Jenn

--- script.js
+++ script.js
@@ -69,37 +69,6 @@ initMap();
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const comentariosList = document.getElementById("comentarios-list");
-
-    // Hacer una solicitud a la API de JSONPlaceholder para obtener comentarios
-    fetch("https://jsonplaceholder.typicode.com/comments")
-        .then((response) => response.json())
-        .then((comentarios) => {
-            // Tomar solo los primeros dos comentarios
-            const comentariosLimitados = comentarios.slice(0, 2);
-
-            // Recorrer los comentarios limitados y agregarlos a la lista en la página
-            comentariosLimitados.forEach((comentario) => {
-                // Hacer una solicitud adicional para obtener el nombre de usuario del comentario
-                fetch(`https://jsonplaceholder.typicode.com/users/${comentario.id}`)
-                    .then((response) => response.json())
-                    .then((usuario) => {
-                        const listItem = document.createElement("li");
-
-                        // Agregar una clase CSS al elemento <li>
-                        listItem.classList.add("opiniones"); // Reemplaza "mi-clase-css" con el nombre de tu clase CSS
-
-                        listItem.innerHTML = `<strong>${usuario.name}</strong>: ${comentario.body}`;
-                        comentariosList.appendChild(listItem);
-                    });
-            });
-        })
-        .catch((error) => {
-            console.error("Error al cargar los comentarios: " + error);
-        });
-});
-
 
 let footer = `
 <footer class="seccion-oscura d-flex flex-column align-items-center justify-content-center">
